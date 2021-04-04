2dfc9fea87f7c847ed8c03a2c87b4fb2fe452ccd
Zorba11 <zorbathegreek111@gmail.com>
2021-04-04 18:46:27 +0530

    button and video styles added

---
+++ script.js
@@ -0,0 +1,17 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select a media stream, pass to video element and play
+
+async function selectMediaStream() {
+    try {
+        // get mediastream data, give that meida object as the src of the video element
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        
+    }
+}
